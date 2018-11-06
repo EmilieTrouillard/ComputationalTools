fda95184029cdf994406d11b8df6d5404d7b6f48
EmilieTrouillard <s172727@student.dtu.dk>
2018-11-06 16:27:01 +0100

    scriot + generated file of the names of all the json files

---
+++ jsonNames.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  6 16:21:47 2018
+
+@author: ubuntu
+"""
+
+file = open('jsonNames.txt', 'w') 
+ 
+for i in range(175980):
+    file.write(str(i) + '_wiki_part.json\n')
+
+ 
+file.close() 
+
+    
