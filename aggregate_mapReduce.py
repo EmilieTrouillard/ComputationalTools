3d7e81f89284b58815f34a64c884eb7a446df947
EmilieTrouillard <s172727@student.dtu.dk>
2018-11-08 17:05:16 +0100

    now able to serialize the output using pickle

--- aggregate_mapReduce.py
+++ aggregate_mapReduce.py
@@ -5,11 +5,18 @@ Created on Tue Nov  6 16:18:06 2018
 
 @author: ubuntu
 """
-
+import mrjob
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 from parser import parseJSON_FROMXML
+import pickle
+from readGraph import readPickled
 
 class aggregateDict(MRJob):
+    # these are the defaults
+    #INPUT_PROTOCOL = mrjob.protocol.RawValueProtocol
+    #INTERNAL_PROTOCOL = mrjob.protocol.JSONProtocol
+    #OUTPUT_PROTOCOL = mrjob.protocol.JSONProtocol
     def mapper(self, _, line):
         fileName = '/media/ubuntu/1TO/DTU/courses/ComputationalToolsForDataScience/ComputationalTools/parsed/' + line
         parsed_dictionnaries, _ = parseJSON_FROMXML(fileName)
@@ -17,12 +24,36 @@ class aggregateDict(MRJob):
     
                     
     def reducer(self, key, values):
-        D = dict()
-        for d in values:
-            for page_id, links in d.items():
-                D[page_id] = links
-        yield(key, D)
+        D = {int(k):links for d in values for k,links in d.items()}
+                
+                
+        #print(serialized)
+        #outfile.close()
+        yield (None, D)
+    
+    """def one_dict(self, _, D):
+        print(D)
+        yield list(D)
+    
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer),
+            MRStep(reducer=self.one_dict)]"""
+    
+        
+#if __name__ == '__main__':
+#    aggregateDict.run()
+#    #outfile = open('/media/ubuntu/1TO/DTU/courses/ComputationalToolsForDataScience/ComputationalTools/parsed/serializeSmall', 'wb')
+#    #pickle.dump(out, outfile)
+#    #outfile.close()
 
-if __name__ == '__main__':
-    aggregateDict.run()
-
+mr_job = aggregateDict(args=['jsonNames100.txt'])
+with mr_job.make_runner() as runner:
+    runner.run()
+    for line in runner.stream_output():
+        key, value = mr_job.parse_output_line(line)
+        dictWithIntKeys = {int(k): links for k, links in value.items()}
+        outfile = open('MapReduce100','wb')
+        pickle.dump(dictWithIntKeys, outfile)
+        outfile.close()
