b02fa407281bc8af9fd9bcaf88691cc0be4c4a42
EmilieTrouillard <s172727@student.dtu.dk>
2018-11-13 13:50:25 +0100

    deserialized file generation

--- readGraph.py
+++ readGraph.py
@@ -16,8 +16,18 @@ def readPickled(fileName):
     return pickledData
 
 # TESTABLE
-if __name__ == '__main__':
-    print('Enter the name of the file that contains the graph:')
-    graphFile = input()
-    
-    print(readPickled(graphFile))
+#if __name__ == '__main__':
+#    print('Enter the name of the file that contains the graph:')
+#    graphFile = input()
+#    
+#    print(readPickled(graphFile))
+
+decoded_data = open('decodedDataAll', 'w')
+
+data = readPickled('MapReduceAll')
+for key, value in data.items():
+    decoded_data.write(str(key) + ' ')
+    for link in value:
+        decoded_data.write(str(link) + ' ')
+    decoded_data.write('\n')
+decoded_data.close()
