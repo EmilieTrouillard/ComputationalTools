26bcba1788ea4d714c4208b313eb5a6c23f7ec37
EmilieTrouillard <s172727@student.dtu.dk>
2018-11-15 14:48:21 +0100

    running mapReduce with new indices

--- preprocessing/graphcreation_mapReduce.py
+++ preprocessing/graphcreation_mapReduce.py
@@ -7,18 +7,19 @@ Created on Tue Nov  6 16:18:06 2018
 """
 
 from mrjob.job import MRJob
-from parser2 import parseJSON_FROMXML
+from parserNoHash import parseJSON_FROMXML
 import pickle
 
-fileName = 'jsonNames2.txt'
+#fileName = '../data/jsonNames.txt'
+fileName = '../jsonNames1.txt'
 
 class aggregateGraph(MRJob):
 
     def mapper(self, _, line):
 
-        print("line = " + line)
-        #fileName = '/media/ubuntu/1TO/DTU/courses/ComputationalToolsForDataScience/ComputationalTools/parsed/' + line
-        fileName = "C:/Users/Finn/Dropbox/02807/project/computationaltools/" + line
+        #print("line = " + line)
+        fileName = '/media/ubuntu/1TO/DTU/courses/ComputationalToolsForDataScience/ComputationalTools/parsed/' + line
+        #fileName = "C:/Users/Finn/Dropbox/02807/project/computationaltools/" + line
 
 
         parsed_dictionnaries, _, _ = parseJSON_FROMXML(fileName)
@@ -33,43 +34,46 @@ class aggregateGraph(MRJob):
 
         nodes = list(values)
 
-        titletoidmap = {}
-        idtotitlemap = {}
+        titleToIdMap = {}
+        idToTitleTap = {}
         counter = 0
-        for i in range(0,len(nodes)):
-            for pagetitle in nodes[i]:
-                titletoidmap[pagetitle] = counter
-                idtotitlemap[counter] = pagetitle
+        for dictOfPages in nodes:
+            for pageTitle in dictOfPages.keys():
+                titleToIdMap[pageTitle] = counter
+                idToTitleTap[counter] = pageTitle
                 counter += 1
 
-        graph = [[i] for i in range(0,counter)] # First number is the id. The remaining numbers are the neighbors.
+        graph = [[i] for i in range(counter)] # First number is the id. The remaining numbers are the neighbors.
 
-        for i in range(0,len(nodes)):
-            for pagetitle in nodes[i]:
-                for neighbortitle in nodes[i][pagetitle]:
+        for dictOfPages in nodes:
+            for pageTitle, links in dictOfPages.items():
+                for neighborTitle in links:
                     #print(str(pagetitle) + ": " + str(neighbortitle))
                     #print(neighbortitle)
-                    if neighbortitle in titletoidmap:
-                        graph[titletoidmap[pagetitle]].append(titletoidmap[neighbortitle])
+                    if neighborTitle in titleToIdMap:
+                        graph[titleToIdMap[pageTitle]].append(titleToIdMap[neighborTitle])
 
 
-        yield None, [graph, titletoidmap, idtotitlemap]
+        yield None, [graph, titleToIdMap, idToTitleTap]
 
 #print(fileName)
 mr_job = aggregateGraph(args=[fileName])
 with mr_job.make_runner() as runner:
     runner.run()
-    with open('graphfile','w') as f1, open('titletoidmap','w') as f2, open('idtotitlemap','w') as f3:
+    with open('graphfile','w') as f1, open('titletoidmap','wb') as f2, open('idtotitlemap','wb') as f3:
 
         for _,value in mr_job.parse_output(runner.cat_output()):
-            for nodelist in value[0]:
-                for i in range(0,len(nodelist)):
-                    f1.write(str(nodelist[i]) + " ")
-                f1.write("\n")
-            for title in value[1]:
-                f2.write(title + " " + str(value[1][title]))
-            for id in value[2]:
-                f3.write(id + " " + value[2][id])
+            s = ' '.join([' '.join(map(str, nodelist)) + '\n' for nodelist in value[0]])
+            f1.write(s)
+#                for i in range(0,len(nodelist)):
+#                    f1.write(str(nodelist[i]) + " ")
+#                f1.write("\n")
+            pickle.dump(value[1], f2)
+            pickle.dump(value[2], f3)
+#            for title in value[1]:
+#                f2.write(title + " " + str(value[1][title]))
+#            for id in value[2]:
+#                f3.write(id + " " + value[2][id])
 
 
             #for line in runner.util.to_lines(runner.cat_output()):
--- preprocessing/jsonNames.py
+++ preprocessing/jsonNames.py
@@ -8,7 +8,7 @@ Created on Tue Nov  6 16:21:47 2018
 
 file = open('jsonNames.txt', 'w') 
  
-for i in range(175980):
+for i in range(175978):
     file.write(str(i) + '_wiki_part.json\n')
 
  
