42c9e6baa0fd613ea97c2c21c6769802e8bf590b
finn <finnfernstroem@gmail.com>
2018-12-07 17:08:57 +0100

    Added combiner to MRAPSP.py

--- apsp/MRAPSP.py
+++ apsp/MRAPSP.py
@@ -11,7 +11,7 @@ class MRAPSP(MRJob):
     def steps(self):
         return [
             MRStep(mapper=self.mapper1, reducer=self.reducer1),
-            MRStep(mapper=self.mapper2, reducer=self.reducer2),
+            MRStep(mapper=self.mapper2, combiner=self.reducer2, reducer=self.reducer2),
         ]
 
     def mapper1(self,_,line):
